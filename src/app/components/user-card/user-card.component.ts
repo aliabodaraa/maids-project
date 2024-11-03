16844411af7aabf234c693086680dcc64301a100
aliabodaraa <abodaraaali50@gmail.com>
2024-11-03 04:16:46 +0300

    add AppHighlightDirective directive

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -5,11 +5,13 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatMenuModule} from '@angular/material/menu';
 import { User } from '../../models/user';
+import { AppHighlightDirective } from '../../directives/app-highlight.directive';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-user-card',
   standalone: true,
-  imports:[CommonModule,RouterModule,MatProgressSpinnerModule,MatMenuModule,MatPaginatorModule]
+  imports:[CommonModule,RouterModule,MatProgressSpinnerModule,MatMenuModule,MatPaginatorModule,AppHighlightDirective,MatTooltip]
 ,
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss',
